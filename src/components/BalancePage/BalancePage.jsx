cfc11b3874001a09c1f6a8247ca898c85e9f9ab6
Ihor Kychak <ihor.kychak@litslink.com>
2021-07-19 14:58:12 +0300

    transactions wrapper

--- src/components/BalancePage/BalancePage.jsx
+++ src/components/BalancePage/BalancePage.jsx
@@ -14,38 +14,37 @@ import {
 } from '../../helpers';
 import NavLink from '../NavLinks/NavLink.jsx';
 import Spinner from '../Spinner/Spinner.jsx';
+import TransactionsWrapper from '../TransactionsWrapper/TransactionsWrapper.jsx';
 
 import './BalancePage.scss';
 
 const BalancePage = () => {
   const list = useSelector(transactionsTotalsSelector);
 
-  if (list.length < 1) {
-    return <Spinner />;
-  }
-
   const totalInputs = totalsCalculation(list, inputs, inputValue);
 
   const totalOutputs = totalsCalculation(list, outputs, outputValue);
 
   return (
-    <div className="balance__page">
-      <h1 className="balance__page_balance">
-        Your current balance is:
-        <span className="main__page_header-span">{` ${
-          totalInputs - totalOutputs
-        } 💲`}</span>
-      </h1>
-      <h3 className="balance__page_spent">
-        Total spent:
-        <span className="main__page_header-span">{` ${totalOutputs} 💲`}</span>
-      </h3>
-      <h3 className="balance__page_received">
-        Total received:
-        <span className="main__page_header-span">{` ${totalInputs} 💲`}</span>
-      </h3>
-      <NavLink text="Home" route={HOME} />
-    </div>
+    <TransactionsWrapper list={list}>
+      <div className="balance__page">
+        <h1 className="balance__page_balance">
+          Your current balance is:
+          <span className="main__page_header-span">{` ${
+            totalInputs - totalOutputs
+          } 💲`}</span>
+        </h1>
+        <h3 className="balance__page_spent">
+          Total spent:
+          <span className="main__page_header-span">{` ${totalOutputs} 💲`}</span>
+        </h3>
+        <h3 className="balance__page_received">
+          Total received:
+          <span className="main__page_header-span">{` ${totalInputs} 💲`}</span>
+        </h3>
+        <NavLink text="Home" route={HOME} />
+      </div>
+    </TransactionsWrapper>
   );
 };
 
--- src/components/TransactionDetails/TransactionDetails.jsx
+++ src/components/TransactionDetails/TransactionDetails.jsx
@@ -7,13 +7,14 @@ import { transactionsTotalsSelector } from '../../redux/modules/transactionsList
 import NavLink from '../NavLinks/NavLink.jsx';
 import TransactionDetailsList from './TransactionDetailsList.jsx';
 import Spinner from '../Spinner/Spinner.jsx';
+import TransactionsWrapper from '../TransactionsWrapper/TransactionsWrapper.jsx';
 
 import { HOME, TRANSACTIONS } from '../../routes';
 
 const TransactionDetails = ({ location }) => {
   const list = useSelector(transactionsTotalsSelector);
 
-  if (list.length < 1) {
+  if (list.length === 0) {
     return <Spinner />;
   }
 
@@ -24,39 +25,39 @@ const TransactionDetails = ({ location }) => {
   const [{ inputs, outputs, confirmed, total, hash }] = lookupTransaction;
 
   return (
-    <div className="transactions_list__page">
-      <div className="transactions_list__item">
-        <span className="transactions_list__item-span">
-          Transaction ID:{' '}
-          <span className="transactions_list__item-info">{hash}</span>{' '}
-        </span>
-        <span className="transactions_list__item-span">
-          Transaction total:{' '}
-          <span className="transactions_list__item-info">{total}</span>
-        </span>
-        <span className="transactions_list__item-span">
-          Date confirmed:{' '}
-          <span className="transactions_list__item-info">
-            {moment(confirmed).format('YYYY-MM-DD HH:mm')}
+      <div className="transactions_list__page">
+        <div className="transactions_list__item">
+          <span className="transactions_list__item-span">
+            Transaction ID:{' '}
+            <span className="transactions_list__item-info">{hash}</span>{' '}
           </span>
-        </span>
+          <span className="transactions_list__item-span">
+            Transaction total:{' '}
+            <span className="transactions_list__item-info">{total}</span>
+          </span>
+          <span className="transactions_list__item-span">
+            Date confirmed:{' '}
+            <span className="transactions_list__item-info">
+              {moment(confirmed).format('YYYY-MM-DD HH:mm')}
+            </span>
+          </span>
 
-        <div>
           <div>
-            <h3>Inputs</h3>
-            <TransactionDetailsList operations={inputs} />
+            <div>
+              <h3>Inputs</h3>
+              <TransactionDetailsList operations={inputs} />
+            </div>
+            <div>
+              <h3>Outputs</h3>
+              <TransactionDetailsList operations={outputs} />
+            </div>
           </div>
-          <div>
-            <h3>Outputs</h3>
-            <TransactionDetailsList operations={outputs} />
+          <div className="main__page_buttons">
+            <NavLink text="Back" route={TRANSACTIONS} />
+            <NavLink text="Home" route={HOME} />
           </div>
-        </div>
-        <div className="main__page_buttons">
-          <NavLink text="Back" route={TRANSACTIONS} />
-          <NavLink text="Home" route={HOME} />
         </div>
       </div>
-    </div>
   );
 };
 
